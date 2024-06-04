f1b3dea6664c7dc76a6f97efed1dcdaa834788b4
yamada <ymwk.sota@gmail.com>
2024-06-04 13:36:30 +0900

    hoge

--- frontend/src/main.zig
+++ frontend/src/main.zig
@@ -8,8 +8,10 @@ const h = elem.createElement(.heading);
 const p = elem.createElement(.p);
 
 pub fn main() !void {
-    const a = p.init(.{ "Hello {s}", .{"paragraph!"} });
-    _ = a;
+    const epoch = std.time.epoch.EpochSeconds{ .secs = @intCast(std.time.timestamp()) };
+    const year = epoch.getEpochDay().calculateYearDay();
+
+    const copyright = p.init(.{ "&copy;{}", .{year.year} });
 
     try render("#app", .{
         comptime Header("zFrame -- Zig Web Backend Framework"),
@@ -17,20 +19,14 @@ pub fn main() !void {
         assert("This is still under development."),
         comptime README(),
 
-        // a.setClass("text-red-100"),
-        // div.init(.{
-        //     // "hoge",
-        //     &div.init(.{
-        //         &p.init(.{ "test: {}", .{1} }),
-        //     }),
-        // }),
-        // h.init(.{ " {s}", .{"タイトルやさかい"} })
-        //     .setClass("text-3xl text-align text-indigo-700"),
-        // h.init(.{"Testや!"}).setClass("text-purple-500"),
+        div.setClass("text-center border-b border-300-cyan").init(.{
+            &div.setClass("pb-2").init(.{
+                &copyright.setClass("text-sm"),
+            }),
+        }),
     });
 }
 
-// 呼び出しをcomptimeで行う必要あり
 fn Header(title: []const u8) Element {
     return div.setClass("text-center my-10").init(.{
         // &h.init(.{title}).setClass("text-4xl text-indigo-700 font-bold"),
@@ -38,21 +34,18 @@ fn Header(title: []const u8) Element {
     });
 }
 
-// inline にしてもいける
 inline fn assert(assert_text: []const u8) Element {
     return div.setClass("mx-auto text-center my-10 bg-indigo-200 w-1/3 border rounded-2xl border-indigo-300").init(.{
         &p.init(.{"[!]\t" ++ assert_text}).setClass("text-lg px-1 py-2 text-gray-600 font-black"),
     });
 }
 
-// HTML直書きもできる
-// 子要素数の問題とか諸々解決はする
 fn README() Element {
     return div.setClass("text-center my-10").init(.{
         &p.setClass("text-lg text-gray-700 font-semibold").init(.{
             \\ The Web framework for mainting robust, optimal and reusable by Zig language.</p>
             \\ <p class="text-lg text-gray-700 font-semibold">This is developed at SecHack365 in 2024.</p>
-            \\ <p class="text-lg text-gray-700 font-semibold">GitHub Repository is here: https://github.com/yamada031016/zframe</p>
+            \\ <p class="text-lg text-gray-700 font-semibold">GitHub Repository is here: https://github.com/yamada031016/zframe
         }),
     });
 }
