936768c2990a869a038f046a35d4bfec22fb1154
yamada031016 <ymwk.sota@gmail.com>
2024-10-08 10:21:26 +0900

    add: test at all file

--- src/root.zig
+++ src/root.zig
@@ -2,3 +2,7 @@ pub const render = @import("render.zig");
 pub const element = @import("element.zig");
 pub const node = @import("node.zig");
 pub const html = @import("html.zig");
+
+comptime {
+    @import("std").testing.refAllDecls(@This());
+}
