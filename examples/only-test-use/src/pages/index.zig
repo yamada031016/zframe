98a92eb93e3a77133469c5709f2bbb7b5af6692f
yamada031016 <ymwk.sota@gmail.com>
2024-12-04 12:04:54 +0900

    stash before breaking change

--- examples/only-test-use/src/pages/index.zig
+++ examples/only-test-use/src/pages/index.zig
@@ -20,16 +20,95 @@ fn index() node.Node {
         node.createNode(.h6).init("h6"),
         p.init("hoge"),
         node.createNode(.a).init(.{ "/", .{}, "index page" }),
-        node.createNode(.a).init(.{ .href = "/about", .template = "about page" }),
+        node.createNode(.a).init(.{
+            .href = "/about",
+            .template = "about page",
+            .target = .blank,
+            .download = "hoge",
+            .rel = .help,
+            .hreflang = "ja",
+            .ping = "/",
+            .referrerpolicy = .noReferrer,
+            .mimeType = "text/html",
+            .attributionsrc = "/",
+        }),
+        node.createNode(.img).init(.{ .src = "", .alt = "hoge" }),
+        node.createNode(.link).init(.{
+            .type = .style,
+            .crossorigin = .anonymous,
+        }),
+        node.createNode(.cite).init("hoge"),
+        node.createNode(.p).init(.{
+            .template = "edit here!",
+            .accesskey = "i",
+            .contenteditable = true,
+            .dir = .leftToRight,
+            .draggable = true,
+            // .hidden = false,
+            .itemprop = "edit",
+            .lang = "ja",
+            .role = "alert",
+            .slot = "hoge",
+            .spellcheck = true,
+            .style = "text:red;",
+            .title = "title",
+            .translate = true,
+        }),
+        node.createNode(.table).init(.{
+            node.createNode(.caption).init("sample table"),
+            node.createNode(.thead).init(.{
+                node.createNode(.tr).init(.{
+                    node.createNode(.th).init("Items"),
+                    node.createNode(.th).init("Expenditure"),
+                }),
+            }),
+            node.createNode(.tbody).init(.{
+                node.createNode(.tr).init(.{
+                    node.createNode(.th).init("Donuts"),
+                    node.createNode(.td).init("3,000"),
+                }),
+                node.createNode(.tr).init(.{
+                    node.createNode(.th).init("Stationery"),
+                    node.createNode(.td).init("18,000"),
+                }),
+            }),
+            node.createNode(.tfoot).init(.{
+                node.createNode(.tr).init(.{
+                    node.createNode(.th).init(.{
+                        .template = "Totals",
+                        // .scope = "row",
+                    }),
+                    node.createNode(.td).init("21,000"),
+                }),
+            }),
+        }),
+        node.createNode(.form).init(.{
+            node.createNode(.div).init(.{
+                node.createNode(.label).init(.{
+                    .template = "Enter your name",
+                    // .forName="name"
+                }),
+                node.createNode(.input).init(.{
+                    // .type = "text",
+                    // .name = "name",
+                    // .required = true,
+                }),
+            }),
+            node.createNode(.input).init(.{
+                // .type = "submit",
+                // .value = "subscribe!",
+            }),
+        }),
     });
 }
 
 pub fn main() !void {
-    const handler = z.handler.JsHandler{
-        .then = .{
-            .filename = "alert.js",
-            .func = "test",
-        },
-    };
-    try z.render.render(@src().file, index().loadWebAssembly("one.wasm", handler));
+    // const handler = z.handler.JsHandler{
+    //     .then = .{
+    //         .filename = "alert.js",
+    //         .func = "test",
+    //     },
+    // };
+    // try z.render.render(@src().file, index().loadWebAssembly("one.wasm", handler));
+    try z.render.render(@src().file, index());
 }
