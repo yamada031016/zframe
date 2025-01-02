4a8a818f9fbacb6cd84b2ed9059cbb9040680f11
yamada031016 <ymwk.sota@gmail.com>
2025-01-02 23:51:48 +0900

    wip

--- zframe-cli/src/init/src/pages/index.zig
+++ zframe-cli/src/init/src/pages/index.zig
@@ -12,13 +12,22 @@ pub fn index() node.Node {
     const span = node.createNode(.span);
 
     return main.setClass("bg-gradient-to-br from-gray-900 via-gray-800 to-gray-700 text-white min-h-screen flex flex-col").init(.{
-        section.setClass("flex flex-col items-center justify-center text-center flex-grow px-4 py-20 space-y-8").init(.{ h1.setClass("text-6xl font-extrabold text-[#4fd1c5] drop-shadow-md").init("Build the Future with zFrame"), p.setClass("text-lg text-gray-300 max-w-2xl").init(.{
-            span.init("Experience a modern web frontend framework powered by "),
-            span.setClass("text-[#63b3ed] font-semibold").init("WebAssembly"),
-            span.init("Create blazing-fast, scalable, and cross-platform web applications effortlessly."),
-        }), a.setClass("px-6 py-3 bg-[#3182ce] text-white font-semibold rounded-lg shadow-lg hover:bg-[#2b6cb0] hover:shadow-2xl transition-all").init(.{
-            .href = "https://github.com/yamada031016/zframe",
-            .template = "Explore zFrame on GitHub",
-        }) }),
+        Head("zframe", .{}),
+        section.setClass("flex flex-col items-center justify-center text-center flex-grow px-4 py-20 space-y-8").init(.{
+            h1
+                .setClass("text-6xl font-extrabold text-[#4fd1c5] drop-shadow-md")
+                .init("Build the Future with zFrame"),
+            p.setClass("text-lg text-gray-300 max-w-2xl").init(.{
+                span.init("Experience a modern web frontend framework powered by "),
+                span.setClass("text-[#63b3ed] font-semibold").init("WebAssembly"),
+                span.init("Create blazing-fast, scalable, and cross-platform web applications effortlessly."),
+            }),
+            a
+                .setClass("px-6 py-3 bg-[#3182ce] text-white font-semibold rounded-lg shadow-lg hover:bg-[#2b6cb0] hover:shadow-2xl transition-all")
+                .init(.{
+                .href = "https://github.com/yamada031016/zframe",
+                .template = "Explore zFrame on GitHub",
+            }),
+        }),
     });
 }
