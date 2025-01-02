d60d32078016501838d1cc9dbba5a64e2df92504
yamada031016 <ymwk.sota@gmail.com>
2025-01-02 23:02:25 +0900

    improve init project

--- zframe-cli/src/init/src/pages/index.zig
+++ zframe-cli/src/init/src/pages/index.zig
@@ -9,59 +9,15 @@ pub fn index() node.Node {
     const p = node.createNode(.p);
     const div = node.createNode(.div);
     const a = node.createNode(.a);
-    // const img = node.createNode(.img);
 
     return div.setClass("text-[#25332a] ").init(.{
-        Head("zframe - Zig Web Frontend Framework", .{}),
-        // img.init(.{.src="hoge", .alt="Test image", .width=100, .height=200}),
-        div.setClass("text-center mt-32").init(.{
-            h1.init(.{"zframe -- Web Frontend Framework"}).setClass("text-5xl text-[#F0544F] font-black"),
-            p.init(.{
-                \\ Utilize Wasm easily, without any configure and technics.<br>
-                \\ zframe enable you to integrate Wasm and your Website.
-            }).setClass("text-xl pt-6"),
-        }),
-        div.setClass("flex justify-center gap-8 pt-24").init(.{
-            a.setClass("py-3 px-6 text-lg font-bold text-white bg-[#F0544F] border").init(.{ "Documentation", .{}, "Get Started" }),
-            a.setClass("py-3 px-6 text-lg font-bold border border-gray-200").init(.{ "Documentation", .{}, "Learn zframe" }),
-        }),
-        a.setClass("text-center block mt-8 text-gray-700 font-light").init(.{"https://github.com/yamada031016/zframe"}),
-        div.setClass("mt-40 bg-[#F0544F] px-28 py-20").init(.{ div.setClass("flex justify-center items-end").init(.{
-            h2.setClass("text-3xl font-extrabold").init(.{"What's in zframe?"}),
-            p.setClass("text-lg pl-4").init(.{"A flexible, fast, robust framework written in Zig"}),
-        }), div.setClass("pt-12").init(.{
-            cardContainer(),
-        }) }),
-    });
-}
+        h1.init("zFrame"),
+        p.init("Modern Web Frontend Framework with WebAssembly"),
+        p.init("you can use and integrate WebAssembly easily"),
 
-fn cardContainer() node.Node {
-    const div = node.createNode(.div).init(.{});
-    const empty = node.createNode(.empty);
+        h2.init("Let's create your website in Zig lang!"),
 
-    const feature_cards = [_][2][]const u8{
-        [_][]const u8{ "Built-in Optimizations", "Automatic Image, Font and Wasm Optimizations are all built-in" },
-        [_][]const u8{ "Tailwind CSS Support", "Automatic Image, Font and Wasm Optimizations are all built-in" },
-        [_][]const u8{ "Integrated Web Assembly", "Automatic Image, Font and Wasm Optimizations are all built-in" },
-        [_][]const u8{ "Built-in Web Server", "Automatic Image, Font and Wasm Optimizations are all built-in" },
-        [_][]const u8{ "File System Based Routing", "Automatic Image, Font and Wasm Optimizations are all built-in" },
-        [_][]const u8{ "Useful Dev Utils", "Automatic Image, Font and Wasm Optimizations are all built-in" },
-    };
-
-    return empty.init(.{
-        inline for (feature_cards) |f| {
-            div.addChild(card(f[0], f[1]));
-        },
-        div.setClass("grid grid-cols-3 gap-5"),
-    });
-}
-
-fn card(title: []const u8, description: []const u8) node.Node {
-    const p = node.createNode(.p);
-    const div = node.createNode(.div);
-
-    return div.setClass("p-4 border border-1 rounded-xl border-gray-200").init(.{
-        p.setClass("text-xl font-bold w-4/5").init(.{title}),
-        p.init(.{description}),
+        h2.init("Some example projects are here!"),
+        a.init(.{ .href = "https://github.com/yamada031016/zframe", .template = "zframe project repository" }),
     });
 }
