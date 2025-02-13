7d19cfd25f28f38edd80cb35c43d2093ee2ea525
yamada031016 <ymwk.sota@gmail.com>
2025-02-13 22:10:18 +0900

    wip

--- build.zig
+++ build.zig
@@ -4,6 +4,14 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const exe = b.addExecutable(.{
+        .name = "zframe",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(exe);
+
     const lib = b.addStaticLibrary(.{
         .name = "zframe",
         .root_source_file = b.path("src/zframe.zig"),
@@ -23,6 +31,7 @@ pub fn build(b: *std.Build) !void {
         .optimize = .ReleaseFast,
     });
     lib.root_module.addImport("markdown-zig", md.module("markdown-zig"));
+    exe.root_module.addImport("markdown-zig", md.module("markdown-zig"));
     zframe_mod.addImport("markdown-zig", md.module("markdown-zig"));
 
     // const wasm_analyzer_optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
