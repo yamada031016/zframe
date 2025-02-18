faa2dc1a3da2a6a8f9c5256716ed4be7a03fee54
yamada031016 <ymwk.sota@gmail.com>
2025-02-18 12:52:34 +0900

    wip

--- examples/homepage/src/main.zig
+++ examples/homepage/src/main.zig
@@ -3,7 +3,7 @@ const zframe = @import("zframe");
 const c = @import("components");
 
 pub fn main() !void {
-    try zframe.render.config(c.layout.Layout, c.head.Head("", .{}));
+    try zframe.render.config(c.layout.Layout, c.head.Head);
     try zframe.render.render("src/pages/index.zig", @import("pages/index.zig").index());
     try zframe.render.render("src/pages/about.zig", @import("pages/about.zig").about());
     // try zframe.render.mdToHtml("src/pages/Docs.md");
