20bcfccd896da3314314b2d86989498c72f449cf
yamada031016 <ymwk.sota@gmail.com>
2025-03-06 16:52:56 +0900

    small change

--- examples/homepage/src/components/footer.zig
+++ examples/homepage/src/components/footer.zig
@@ -10,7 +10,7 @@ pub fn Footer() Node {
     const epoch = std.time.epoch.EpochSeconds{ .secs = @intCast(std.time.timestamp()) };
     const year = epoch.getEpochDay().calculateYearDay();
 
-    const copyright = div.init(.{ "&copy;{} zFrame.", .{year.year} });
+    const copyright = div.init(.{ "&copy;{} zframe", .{year.year} });
 
     return footer.setClass("py-8 text-center text-gray-600 dark:text-gray-400 bg-gray-100 dark:bg-gray-800").init(.{
         copyright,
